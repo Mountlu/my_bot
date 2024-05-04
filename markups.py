29d759750c53babe1b1438ed431830d778c37792
Mountlu <rikirikiriki2412@gmail.com>
2024-05-04 13:39:57 +0300

    Create markups.py

---
+++ markups.py
@@ -0,0 +1,14 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton 
+ 
+btnMain = KeyboardButton("Главное меню") 
+ 
+# --- Main Menu --- 
+btnRandom = KeyboardButton("Рандомное число") 
+btnOther = KeyboardButton("Другое") 
+mainMenu = ReplyKeyboardMarkup(resize_keyboard = True).add(btnOther, btnRandom) 
+ 
+ 
+# --- Other Menu --- 
+btnInfo = KeyboardButton("Информация") 
+btnMoney = KeyboardButton("Курсы валют") 
+otherMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btnInfo, btnMoney, btnMain)
