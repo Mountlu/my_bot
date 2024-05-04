790564b903afd7b2b0a941bec59775a11eebefaf
Mountlu <rikirikiriki2412@gmail.com>
2024-05-04 14:18:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,29 @@ import random
  
 TOKEN = "7020597798:AAGhv-EzyiM8FuG5RbqKYvj1lcTt9jlL86g"
  
-bot = Bot(token = TOKEN)  
-dp = Dispatcher(bot) 
-      
+async def start():
+    bot = Bot(token = TOKEN)  
+    dp = Dispatcher(bot) 
+
+menu_keyboard = types.InlineKeyboardMarkup(row_width=2)
+menu_keyboard(
+    types.InlineKeyboardButton("Раздел 1", callback_data="section_1"),
+    types.InlineKeyboardButton("Раздел 2", callback_data="section_2"),
+    types.InlineKeyboardButton("Раздел 3", callback_data="section_3")
+
 @dp.message_handler(commands=['start'])
 async def command_start(message: types.Message): 
-    await bot.send_message(message.from_user.id, "Привет {0.first_name}".format(message.from_user),reply_markup=nav.mainMenu)  
+    await message.reply('Привет! Я бот!')
+
+@dp.message_handler(commands=['help'])
+async def command_start(message: types.Message): 
+    await bot.send_message("Выберите раздел", reply_markup=menu_keyboard)
+
+@dp.callback_query_handler(lambda c: c.data.startwith("section_"))
+async def process_menu_selection(callback_query: types.CallbackQuery):
+    section = callback_query.data.split("_")[1]
+    await bot.answer_callback_query(callback_query.id)
+    await bot.send_message(callback_query.from_user.id, "Вы выбрали раздел" (section)
  
 @dp.message_handler() 
 async def bot_message(message: types.Message): 
@@ -27,6 +44,11 @@ async def bot_message(message: types.Message):
 
     else:
         await message.reply("Неизвестная команда", reply_markup = nav.mainMenu) 
+
+    try:
+        await dp_start_polling(bot)
+    finally:
+        await bot.session.close()
 
 if __name__ == '__main__':  
     executor.start_polling(dp, skip_updates = True)
